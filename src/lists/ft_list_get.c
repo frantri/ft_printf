f5e06a6cbcb370527459f1c49faaa8cb756dc926
Francois Triquet <francois.triquet@gmail.com>
2016-02-27 12:50:51 +0100

    fix ft_list_get_at

--- src/lists/ft_list_get.c
+++ src/lists/ft_list_get.c
@@ -6,7 +6,7 @@
 /*   By: ftriquet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/27 10:41:13 by ftriquet          #+#    #+#             */
-/*   Updated: 2016/02/27 12:25:55 by ftriquet         ###   ########.fr       */
+/*   Updated: 2016/02/27 12:50:39 by ftriquet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,31 +54,22 @@ void		*ft_list_get_at(
 		t_list *list,
 		size_t pos)
 {
-	//static size_t		previous_pos = 0;
+	static size_t		previous_pos = 0;
 	static t_list_cell	*cursor = NULL;
-	size_t				i;
 
-	i = 0;
 	if (pos >= list->size)
 		return (NULL);
-	cursor = list->first;
-	while (i < pos)
+	if (cursor == NULL)
 	{
-		cursor = cursor->next;
-		++i;
+		previous_pos = 0;
+		cursor = list->first;
 	}
-	return (cursor->data);
-//	if (cursor == NULL)
-//	{
-//		previous_pos = 0;
-//		cursor = list->first;
-//	}
-//	while (pos != previous_pos)
-//	{
-//		cursor = pos > previous_pos ? cursor->prev : cursor->next;
-//		previous_pos += pos > previous_pos ? 1 : -1;
-//	}
-//	if (cursor)
-//		return (cursor->data);
+	while (pos != previous_pos)
+	{
+		cursor = pos > previous_pos ? cursor->prev : cursor->next;
+		previous_pos += pos > previous_pos ? 1 : -1;
+	}
+	if (cursor)
+		return (cursor->data);
 	return (NULL);
 }
