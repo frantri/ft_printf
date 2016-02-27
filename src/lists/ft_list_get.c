13e6aa6f1de6532e61380dcd511ef8463a8e0e99
Francois Triquet <francois.triquet@gmail.com>
2016-02-27 13:41:35 +0100

    final fix list_get_at

--- src/lists/ft_list_get.c
+++ src/lists/ft_list_get.c
@@ -6,7 +6,7 @@
 /*   By: ftriquet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/27 10:41:13 by ftriquet          #+#    #+#             */
-/*   Updated: 2016/02/27 12:50:39 by ftriquet         ###   ########.fr       */
+/*   Updated: 2016/02/27 13:41:18 by ftriquet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,20 +54,17 @@ void		*ft_list_get_at(
 		t_list *list,
 		size_t pos)
 {
-	static size_t		previous_pos = 0;
-	static t_list_cell	*cursor = NULL;
+	size_t		i;
+	t_list_cell	*cursor;
 
 	if (pos >= list->size)
 		return (NULL);
-	if (cursor == NULL)
+	cursor = list->first;
+	i = 0;
+	while (i < pos)
 	{
-		previous_pos = 0;
-		cursor = list->first;
-	}
-	while (pos != previous_pos)
-	{
-		cursor = pos > previous_pos ? cursor->prev : cursor->next;
-		previous_pos += pos > previous_pos ? 1 : -1;
+		++i;
+		cursor = cursor->next;
 	}
 	if (cursor)
 		return (cursor->data);
